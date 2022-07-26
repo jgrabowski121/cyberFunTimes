6d54653b0f740236012ef9280a483e973af1a60a
jgrabowski121 <justingrabowski@yahoo.com>
2022-07-26 10:22:13 -0400

    Update temp_converter.py

--- Python/temp_converter.py
+++ Python/temp_converter.py
@@ -5,15 +5,21 @@
 
 
 while(True): 
-    input_temperature = int(input("Please enter the temperature you wish to convert: "))
-    input_metric = input("Please enter the current metric: ")
+    try:
+        input_temperature = int(input("Please enter the temperature you wish to convert (ctrl-c to exit): "))
+        input_metric = input("Please enter the current metric: ")
 
-    if input_metric.lower() == 'c':
-        result = (input_temperature * 9 / 5) + 32
-        print("{} C is equal to {} F".format(input_temperature, result))
-    elif input_metric.lower() == 'f':
-        result = (input_temperature - 32) * 5 / 9 
-        print("{} F is equal to {} C".format(input_temperature, result))
-    else:
-        print("Invalid metric")
-
+        if input_metric.lower() == 'c':
+            result = (input_temperature * 9 / 5) + 32
+            print("{} C is equal to {} F".format(input_temperature, result))
+        elif input_metric.lower() == 'f':
+            result = (input_temperature - 32) * 5 / 9 
+            print("{} F is equal to {} C".format(input_temperature, result))
+        else:
+            print("Invalid metric")
+    
+    except KeyboardInterrupt:
+        print(" ..Program Exited")
+        break
+    except: 
+        print("Do as I say asshole!")
